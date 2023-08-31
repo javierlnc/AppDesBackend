42be92fcceed0c5cb57331b71ff5c95a13c5e277
Javier <javierlnc@icloud.com>
2023-08-31 19:02:54 +0000

    Creacion modelos

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,98 @@
+package org.example.model;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails{
+    private static final long serialVersionUID = 3592549577903104696L;
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private List<String> authorityNames = new ArrayList<String>();
+    private String username;
+    @JsonIgnore
+    private String password;
+
+    public String getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public String getFirstName(){
+        retunr firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getlastName(){
+        return lastName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public List<String> getAuthorityNames() {
+		return authorityNames;
+	}
+	public void setAuthorityNames(List<String> authorityNames) {
+		this.authorityNames = authorityNames;
+	}
+    public String getUsername(){
+        return username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> list = authorityNames
+				.stream()
+				.map(auth -> new SimpleGrantedAuthority(auth))
+				.collect(Collectors.toList());
+
+		return list;
+	}
+
+    
+}
